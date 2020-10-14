38a40f92867cfa8d58cd4f066ffc5a4784d1046b
huckbit <huckbit@gmail.com>
2020-10-14 14:39:20 +0100

    theme persistent in ls

--- src/context/themeContext.js
+++ src/context/themeContext.js
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 const ThemeContext = React.createContext();
 
 function ThemeContextProvider(props) {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("");
+
+  //first execution gets value from localStorage if doesn't exists set to light
+  useEffect(() => {
+    const localTheme = localStorage.getItem("theme");
+    localTheme ? setTheme(localTheme) : setTheme("light");
+  }, []);
+
+  //store value in localstorage when theme changes
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   function toggleTheme() {
     setTheme(theme === "light" ? "dark" : "light");
