bb1958c19566af7083e6985bd604188dcceb7f64
huckbit <huckbit@gmail.com>
2020-10-14 17:42:58 +0100

    gh-pages

---
+++ precache-manifest.4aa2f446dcde413571af18dc535c98e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e9c6ba97b573c0640c4a03256b8865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4640c92bbada116713f4",
+    "url": "/static/css/main.68a37ef3.chunk.css"
+  },
+  {
+    "revision": "d3e6df4b9fa46e15289e",
+    "url": "/static/js/2.f31b96ae.chunk.js"
+  },
+  {
+    "revision": "1a4ff882ab3126b211e28b2b9daed574",
+    "url": "/static/js/2.f31b96ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4640c92bbada116713f4",
+    "url": "/static/js/main.73826d15.chunk.js"
+  },
+  {
+    "revision": "b1420313da0624519db2",
+    "url": "/static/js/runtime-main.fe484e42.js"
+  }
+]);
