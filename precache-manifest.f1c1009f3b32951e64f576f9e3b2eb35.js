930040930f3cb8de6b74dd9eb9d53061d945afbe
huckbit <huckbit@gmail.com>
2024-02-19 12:21:54 +0000

    gh-pages

---
+++ precache-manifest.f1c1009f3b32951e64f576f9e3b2eb35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1692a73b39ab2fc66b7e753153ec443",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bd30afd3c4aab0248c4",
+    "url": "/static/css/main.fba695d7.chunk.css"
+  },
+  {
+    "revision": "5943e3a20900650d1013",
+    "url": "/static/js/2.ea674e9d.chunk.js"
+  },
+  {
+    "revision": "1a4ff882ab3126b211e28b2b9daed574",
+    "url": "/static/js/2.ea674e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd30afd3c4aab0248c4",
+    "url": "/static/js/main.4d4453ae.chunk.js"
+  },
+  {
+    "revision": "b1420313da0624519db2",
+    "url": "/static/js/runtime-main.fe484e42.js"
+  }
+]);
